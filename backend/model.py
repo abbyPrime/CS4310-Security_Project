a95beaa7f453974169737c5d9cea7d162a12c9ec
jeonghoyang12 <wjdwjdgh6998@gmail.com>
2026-04-03 11:25:08 -0400

    udpate imports

--- backend/model.py
+++ backend/model.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Timestamp, Boolean, Text, ForeignKey
+from sqlalchemy import Column, Integer, String, Timestamp, ForeignKey
 from sqlalchemy.sql import func
 from sqlalchemy.orm import relationship
 from database import Base
